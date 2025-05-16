d899f79bfffdd699b5a2c7601bd761de34fc0252
Verfressenq <oeffel22@gmx.de>
2025-05-16 22:10:16 +0200

    Ich hab alles gemacht du krüppel
    - Movement System added
    - Camera Smoothing added
    - Tilemap/2D Environment system added

--- Imfo Projekt fr/Assets/Scripts/PlayerMovement.cs
+++ Imfo Projekt fr/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
-        transform.position = position;
-    }
+public class PlayerController : MonoBehaviour
+{
+
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 32f;
+    private bool isFacingRight = true;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+   void Update()
+   {
+       horizontal = Input.GetAxisRaw("Horizontal");
+
+       if (Input.GetButtonDown("Jump") && IsGrounded())
+       {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+       }
+
+       if (Input.GetButtonDown("Jump") && rb.linearVelocity.y > 0f)
+       {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+       }
+
+       Flip();
+   }
+
+   private void FixedUpdate()
+   {
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+   }
+
+   private bool IsGrounded()
+   {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+   }
+
+   private void Flip()
+   {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+   }
 }
