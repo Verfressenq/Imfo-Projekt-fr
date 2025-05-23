f7cc896e378c73c9100c797da6860e8e4deb228e
Verfressenq <oeffel22@gmx.de>
2025-05-23 21:49:37 +0200

    oriut soiud hgSD

--- Imfo Projekt fr/Assets/Scripts/Enemy.cs
+++ Imfo Projekt fr/Assets/Scripts/Enemy.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-
+    public Animator animator;
     public int maxHealth = 100;
     int currentHealth;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -17,6 +17,8 @@ public class Enemy : MonoBehaviour
 
         // play hurt animation
 
+        animator.SetTrigger("Hurt"); 
+
         if (currentHealth <= 0)
         {
             Die();
@@ -28,8 +30,11 @@ public class Enemy : MonoBehaviour
 
         Debug.Log("Enemy died!");
         // Death animation
+        animator.SetBool("isDead", true);
 
         //disable enemy
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
 
     }
 }
