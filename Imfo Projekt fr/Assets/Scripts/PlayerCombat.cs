21a3b53a4082caf67c00490e1ba58a2991c7a6df
Leonegeabschieber <myplayingemailforme@gmail.com>
2025-06-06 20:20:23 +0200

    Revert "Merge branch 'main' of https://github.com/Verfressenq/Imfo-Projekt-fr"
    This reverts commit 6d65ba4704fafde1860424212749dde7b07cc893, reversing
    changes made to f85b3e91aed91211e64d1f7ea5b927511c1861c9.

--- Imfo Projekt fr/Assets/Scripts/PlayerCombat.cs
+++ Imfo Projekt fr/Assets/Scripts/PlayerCombat.cs
@@ -1,43 +1,79 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCombat : MonoBehaviour
 {
-
-
     public Animator animator;
     public Transform attackPoint;
     public float attackRange = 0.5f;
     public LayerMask enemyLayers;
-    public int attackDamage = 40;
 
-    // Update is called once per frame
+    public int attackDamage = 25;
+    public GameObject fireballPrefab;
+    public Transform fireballSpawnPoint;
+
+    public float swordCooldown = 1f;
+    public float fireballCooldown = 10f;
+
+    private float nextSwordTime = 0f;
+    private float nextFireballTime = 0f;
+
+    // UI references (assign in inspector)
+    public Image swordCooldownImage;
+    public Image fireballCooldownImage;
+
+    private int attackIndex = 0;
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        float time = Time.time;
+
+        // Sword attack (LMB)
+        if (Input.GetMouseButtonDown(0) && time >= nextSwordTime)
         {
-            Attack();
+            nextSwordTime = time + swordCooldown;
+            attackIndex = (attackIndex + 1) % 2;
+            animator.SetTrigger("Attack" + attackIndex);
+
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                enemy.GetComponent<Health>()?.TakeDamage(attackDamage);
+            }
         }
+
+        // Fireball (RMB)
+        if (Input.GetMouseButtonDown(1) && time >= nextFireballTime)
+        {
+            nextFireballTime = time + fireballCooldown;
+            animator.SetTrigger("Fireball");
+
+            Instantiate(fireballPrefab, fireballSpawnPoint.position, transform.rotation);
+        }
+
+        UpdateCooldownUI();
     }
 
-    void Attack()
+    void UpdateCooldownUI()
     {
-        //play attack animation
-        animator.SetTrigger("Attack");
-        //detect enemies in range of attack
-        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
-        //damage deal
-        foreach (Collider2D enemy in hitEnemies)
+        float time = Time.time;
+
+        if (swordCooldownImage)
         {
-            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+            float swordRemaining = Mathf.Clamp01((nextSwordTime - time) / swordCooldown);
+            swordCooldownImage.fillAmount = swordRemaining;
         }
 
+        if (fireballCooldownImage)
+        {
+            float fireballRemaining = Mathf.Clamp01((nextFireballTime - time) / fireballCooldown);
+            fireballCooldownImage.fillAmount = fireballRemaining;
+        }
     }
 
     void OnDrawGizmosSelected()
     {
-        if (attackPoint == null)
-            return;
-
-        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+        if (attackPoint != null)
+            Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
